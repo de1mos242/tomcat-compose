26b7e7c0ff128c69ec6bb46cb3d957d67ea4ddbf
de1mos <de1m0s242@gmail.com>
2017-06-28 11:39:26 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ envsubst '$TOMCAT_USER $TOMCAT_PASSWORD' < /usr/local/tomcat/conf/tomcat-users.x
 echo 'Entrypoint refreshed'
 echo 'tomcat-users result conf:'
 cat /usr/local/tomcat/conf/tomcat-users.xml
+echo \<?xml version=\"1.0\" encoding=\"UTF-8\"?\>\<Context antiResourceLocking=\"false\" privileged=\"true\" \>\</Context\> > /usr/local/tomcat/webapps/manager/META-INF/context.xml
+cat /usr/local/tomcat/webapps/manager/META-INF/context.xml
 
 exec "$@"
